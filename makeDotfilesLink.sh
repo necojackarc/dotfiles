de373c74e16315a184acf5981b9757e881d4e074
necojackarc <necojackarc@gmail.com>
2015-02-05 10:33:38 +0900

    Improve .gitignore and rename the shellscrypt file

--- makeDotfilesLink.sh
+++ makeDotfilesLink.sh
@@ -4,5 +4,3 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.gvimrc ~/.gvimrc
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.inputrc ~/.inputrc
-
-mkdir -p ~/dotfiles/.vim/undo 2>/dev/null
