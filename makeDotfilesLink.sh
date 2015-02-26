3fcb0146ffbcca577021ca674aba2891ceb5ac11
necojackarc <necojackarc@gmail.com>
2015-02-26 12:55:48 +0900

    Add .tmux.conf

--- makeDotfilesLink.sh
+++ makeDotfilesLink.sh
@@ -2,5 +2,6 @@
 ln -sfn ~/dotfiles/.vim ~/.vim
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.gvimrc ~/.gvimrc
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.inputrc ~/.inputrc
