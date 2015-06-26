2c0e955fc60ab0b54f0aa7db23cd6915a60f4bd8
necojackarc <necojackarc@gmail.com>
2015-06-26 18:12:10 +0900

    Rename some dotfiles

--- makeDotfileLinks.sh
+++ makeDotfileLinks.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-ln -sfn ~/dotfiles/.vim ~/.vim
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.gvimrc ~/.gvimrc
-ln -sf ~/dotfiles/.ideavimrc ~/.ideavimrc
-ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
-ln -sf ~/dotfiles/.inputrc ~/.inputrc
+ln -sfn ~/dotfiles/vim ~/.vim
+ln -sf ~/dotfiles/vimrc ~/.vimrc
+ln -sf ~/dotfiles/gvimrc ~/.gvimrc
+ln -sf ~/dotfiles/ideavimrc ~/.ideavimrc
+ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/bash_profile ~/.bash_profile
+ln -sf ~/dotfiles/inputrc ~/.inputrc
