4a494571ed51639be31ae860d10746d144f4fa5e
necojackarc <necojackarc@gmail.com>
2017-03-14 00:41:25 +0900

    Delete ideavimrc

--- setup.sh
+++ setup.sh
@@ -6,7 +6,6 @@ ln -sf ~/dotfiles/gitconfig ~/.gitconfig
 ln -sfn ~/dotfiles/vim ~/.vim
 ln -sf ~/dotfiles/vimrc ~/.vimrc
 ln -sf ~/dotfiles/gvimrc ~/.gvimrc
-ln -sf ~/dotfiles/ideavimrc ~/.ideavimrc
 ln -sfn ~/dotfiles/tmux ~/.tmux
 ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/gemrc ~/.gemrc
