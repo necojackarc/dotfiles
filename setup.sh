ffe051f9a8157d276695be24daf697d14c87e32a
necojackarc <necojackarc@gmail.com>
2018-06-19 03:32:32 +0000

    add my own .gitignore

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ ln -sf ~/dotfiles/profile ~/.profile
 ln -sf ~/dotfiles/bashrc ~/.bashrc
 ln -sf ~/dotfiles/inputrc ~/.inputrc
 ln -sf ~/dotfiles/gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/gitignore ~/.gitignore
 ln -sf ~/dotfiles/editorconfig ~/.editorconfig
 ln -sfn ~/dotfiles/vim ~/.vim
 ln -sf ~/dotfiles/vimrc ~/.vimrc
