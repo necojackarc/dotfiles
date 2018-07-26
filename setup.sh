4da905de5362b5f38ee81518802d8bbc72af455a
necojackarc <necojackarc@gmail.com>
2018-07-26 12:28:20 +0100

    add ignore for ripgrep

--- setup.sh
+++ setup.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
-ln -sf ~/dotfiles/profile ~/.profile
 ln -sf ~/dotfiles/bashrc ~/.bashrc
-ln -sf ~/dotfiles/inputrc ~/.inputrc
+ln -sf ~/dotfiles/editorconfig ~/.editorconfig
+ln -sf ~/dotfiles/gemrc ~/.gemrc
 ln -sf ~/dotfiles/gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/gitignore ~/.gitignore
-ln -sf ~/dotfiles/editorconfig ~/.editorconfig
-ln -sfn ~/dotfiles/vim ~/.vim
-ln -sf ~/dotfiles/vimrc ~/.vimrc
 ln -sf ~/dotfiles/gvimrc ~/.gvimrc
-ln -sfn ~/dotfiles/tmux ~/.tmux
-ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
-ln -sf ~/dotfiles/gemrc ~/.gemrc
+ln -sf ~/dotfiles/ignore ~/.ignore # for ripgrep
+ln -sf ~/dotfiles/inputrc ~/.inputrc
+ln -sf ~/dotfiles/profile ~/.profile
 ln -sf ~/dotfiles/pryrc ~/.pryrc
 ln -sf ~/dotfiles/rspec ~/.rspec
+ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/vimrc ~/.vimrc
+ln -sfn ~/dotfiles/tmux ~/.tmux
+ln -sfn ~/dotfiles/vim ~/.vim
